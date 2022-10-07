00d4a3fc27a114be4141c2696dc84973ada0cd72
Sijaann <97155560+Sijaann@users.noreply.github.com>
2022-10-07 15:53:49 +0545

    abcd

--- lib/screens/navPages/search_page.dart
+++ lib/screens/navPages/search_page.dart
@@ -6,7 +6,7 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Search Page"),
+      child: Center(child: Text("Search Page")),
     );
   }
 }
