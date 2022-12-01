31b607afc06f5b9c9afcb544d927827000ea0e39
Sijaann <97155560+Sijaann@users.noreply.github.com>
2022-12-01 07:05:08 +0545

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:travel_app/screens/details_page.dart';
-import 'package:travel_app/screens/navPages/main_page.dart';
 import 'package:travel_app/screens/on_boarding_screen.dart';
 
 void main() {
